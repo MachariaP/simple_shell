cf78f314f9dce6fa833a682a727263d41dffd6c8
MachariaP <walburphinehas78@gmail.com>
2023-11-10 16:13:42 +0300

    modified file to execute my shell

--- execute.c
+++ execute.c
@@ -1,24 +1,80 @@
 #include "shell.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-int _execute(char **command, char **argv)
+/**
+ * _execute - Executes a command in a child process.
+ * @command: An array of strings representing the command and its arguments.
+ * @argv: An array of strings representing the command line arguments.
+ * @idx: The index of the command in the input.
+ *
+ * Return: The exit status of the executed command.
+ */
+int _execute(char **command, char **argv, int idx)
 {
-    pid_t child;
-    int status;
+	char *full_cmd;
+	pid_t child;
+	int status;
 
-    child = fork();
-    if (child == 0)
-    {
-        if (execve(command[0], command, environ) == -1)
-        {
-            perror(argv[0]);
-            free_2D_array(command);
-	    exit(0);
-        }
-    }
-    else
-    {
-        waitpid(child, &status, 0);
-        free_2D_array(command);
-    }
-    return (WEXITSTATUS(status));
+	/* Path Resolution */
+	full_cmd = _getpath(command[0]);
+	if (!full_cmd)
+	{
+		/* Command not found */
+		print_error(argv[0], command[0], idx);
+		freearray2D(command);
+		return 127;
+	}
+
+	/* Forking a Child Process */
+	child = fork();
+	if (child == -1)
+	{
+		/* Error in forking */
+		perror("fork");
+		free(full_cmd);
+		freearray2D(command);
+		return -1;
+	}
+
+	/* Child Process Execution */
+	if (child == 0)
+	{
+		if (execve(full_cmd, command, environ) == -1)
+		{
+			/* Exit child process on failure */
+			perror("execve");
+			free(full_cmd);
+			freearray2D(command);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		/* Parent Process */
+		if (waitpid(child, &status, 0) == -1)
+		{
+			/* Error in waitpid */
+			perror("waitpid");
+			free(full_cmd);
+			freearray2D(command);
+			return -1;
+		}
+	}
+
+	/* Cleanup */
+	freearray2D(command);
+	free(full_cmd);
+
+	/* Check child process exit status */
+	if (WIFEXITED(status))
+	{
+		return WEXITSTATUS(status);
+	}
+	else
+	{
+		/* Child process did not terminate normally */
+		return -1;
+	}
 }
