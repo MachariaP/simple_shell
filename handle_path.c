0bfe505bbc4beed4ecab1142ed13092cc0819053
MachariaP <walburphinehas78@gmail.com>
2023-11-15 22:39:36 +0300

    modified the getpath func

--- handle_path.c
+++ handle_path.c
@@ -1,85 +1,51 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 
-/**
- * _getpath - Resolves the full path of a command.
- * @command: The command to resolve.
- *
- * Return: A dynamically allocated string representing the full path,
- *         or NULL if the command is not found or encounters an error.
- */
 char *_getpath(char *command)
 {
-    char *path_env = _getenv("PATH");
-    char *full_cmd = NULL, *dir = NULL;
+    char *path_env, *full_cmd, *dir;
+    int i;
     struct stat st;
 
-    if (command == NULL || path_env == NULL)
+    /* if the command is already path */
+    for (i = 0; command[i]; i++)
     {
-        free(path_env);
-        return NULL;
-    }
+        if (command[i] == '/')
+        {
+            /* if path exists */
+            if (stat(command, &st) == 0)
+                return (_strdup(command));
 
-    /* If the command is already a path */
-    if (strchr(command, '/') != NULL)
-    {
-        if (stat(command, &st) == 0) /* If path exists */
-        {
-            free(path_env);
-            return _strdup(command);
-        }
-        else
-        {
-            perror("stat");
-            free(path_env);
-            return NULL;
+            return (NULL);
         }
     }
-    else
+
+    /* if the user unset path */
+    path_env = _getenv("PATH");
+    if (!path_env)
+        return (NULL);
+
+    /* Handling path */
+    dir = strtok(path_env, ":");
+
+    while (dir)
     {
-        /* Handle the path */
-        dir = strtok(path_env, ":");
-        while (dir)
+        /* size = len(directory) + len(command) + 2 ('/' and '\0')*/
+        full_cmd = malloc(_strlen(dir) + _strlen(command) + 2);
+        if (full_cmd)
         {
-            /* size = len(directory) + len(command) + 2 ('/' and '\0') */
-            full_cmd = malloc(_strlen(dir) + _strlen(command) + 2);
-            if (full_cmd)
+            _strcpy(full_cmd, dir);
+            _strcat(full_cmd, "/");
+            _strcat(full_cmd, command);
+            if (stat(full_cmd, &st) == 0)
             {
-                _strcpy(full_cmd, dir);
-
-                if (full_cmd[_strlen(full_cmd) - 1] != '/')
-                {
-                    _strcat(full_cmd, "/");
-                }
-
-                _strcat(full_cmd, command);
-
-                if (stat(full_cmd, &st) == 0)
-                {
-                    free(path_env);
-                    return full_cmd;
-                }
-
-                perror("stat");
-                free(full_cmd);
-            }
-            else
-            {
-                perror("malloc");
                 free(path_env);
-                return NULL;
+                return (full_cmd);
             }
+            free(full_cmd), full_cmd = NULL;
 
             dir = strtok(NULL, ":");
         }
-
-        free(path_env);
     }
-
-    return NULL; /* Command not found */
+    free(path_env);
+    return (NULL);
 }
