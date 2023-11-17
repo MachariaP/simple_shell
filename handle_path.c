4ef5fa98e8aa1724e384b1f1c40c2525a08c882d
MachariaP <walburphinehas78@gmail.com>
2023-11-17 14:42:33 +0300

    modified the handle_path file

--- handle_path.c
+++ handle_path.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * _getpath - Resolves the full path of a command.
  * @command: The command to resolve.
@@ -8,44 +9,56 @@
  */
 char *_getpath(char *command)
 {
-	char *path_env, *full_cmd, *dir;
-	int i;
-	struct stat st;
+        char *path_env, *full_cmd, *dir;
+        int i;
+        struct stat st;
 
-	for (i = 0; command[i]; i++)
-	{
-		if (command[i] == '/')
-		{
-			if (stat(command, &st) == 0)
-				return (_strdup(command));
+        /* Check if the command includes a '/' */
+        for (i = 0; command[i]; i++)
+        {
+                if (command[i] == '/')
+                {
+                        if (stat(command, &st) == 0)
+                                return (_strdup(command));
 
-			return (NULL);
-		}
-	}
-	path_env = _getenv("PATH");
-	if (!path_env)
-		return (NULL);
+                        return (NULL);
+                }
+        }
 
-	dir = strtok(path_env, ":");
+        /* Get the PATH environment variable */
+        path_env = _getenv("PATH");
+        if (!path_env)
+                return (NULL);
 
-	while (dir)
-	{
-		full_cmd = malloc(_strlen(dir) + _strlen(command) + 2);
-		if (full_cmd)
-		{
-			_strcpy(full_cmd, dir);
-			_strcat(full_cmd, "/");
-			_strcat(full_cmd, command);
-			if (stat(full_cmd, &st) == 0)
-			{
-				free(path_env);
-				return (full_cmd);
-			}
-			free(full_cmd), full_cmd = NULL;
+        /* Iterate through directories in PATH */
+        for (dir = strtok(path_env, ":"); dir; dir = strtok(NULL, ":"))
+        {
+                /* Allocate memory for the full command path */
+                full_cmd = malloc(_strlen(dir) + _strlen(command) + 2);
+                if (!full_cmd)
+                {
+                        free(path_env);
+                        return (NULL); /* Memory allocation failed */
+                }
 
-			dir = strtok(NULL, ":");
-		}
-	}
-	free(path_env);
-	return (NULL);
+                /* Construct the full command path */
+                _strcpy(full_cmd, dir);
+                _strcat(full_cmd, "/");
+                _strcat(full_cmd, command);
+
+                /* Check if the full command path exists */
+                if (stat(full_cmd, &st) == 0)
+                {
+                        free(path_env);
+                        return (full_cmd); /* Return the full command path */
+                }
+
+                /* Free the allocated memory for full_cmd */
+                free(full_cmd);
+        }
+
+        /* Free the PATH environment variable */
+        free(path_env);
+
+        return (NULL); /* Command not found in PATH */
 }
